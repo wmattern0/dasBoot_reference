4223da57d67d4d4faadda96ea1edffecd8ebb67d
wmattern0 <wm.m.mattern@gmail.com>
2017-03-16 16:32:52 -0400

    init tests

---
+++ src/test/java/com/boot/controller/ShipwreckControllerTest.java
@@ -0,0 +1,27 @@
+package com.boot.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by williammattern on 3/16/17.
+ */
+public class ShipwreckControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void list() throws Exception {
+
+    }
+
+    @Test
+    public void get() throws Exception {
+
+    }
+
+}
