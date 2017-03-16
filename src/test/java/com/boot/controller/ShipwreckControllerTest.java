65b7d02930e28c4fe23792335bed11d2d0cc1c2f
wmattern0 <wm.m.mattern@gmail.com>
2017-03-16 17:25:27 -0400

    added test before method

--- src/test/java/com/boot/controller/ShipwreckControllerTest.java
+++ src/test/java/com/boot/controller/ShipwreckControllerTest.java
@@ -2,6 +2,11 @@ package com.boot.controller;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.Assert.*;
 
@@ -9,9 +14,16 @@ import static org.junit.Assert.*;
  * Created by williammattern on 3/16/17.
  */
 public class ShipwreckControllerTest {
+
+    @InjectMocks
+    private ShipwreckController shipwreckController;
+
+    private MockMvc mockMvc;
+
     @Before
     public void setUp() throws Exception {
-
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(shipwreckController).build();
     }
 
     @Test
